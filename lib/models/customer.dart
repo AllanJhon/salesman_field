b2d0a171d5824324b4a93159811f354689a50c60
YingLe <yingle1991@gmail.com>
2019-07-13 01:57:21 +0800

    增加客户调用后端接口

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,18 +1,21 @@
+import 'package:xml/xml.dart' as xml;
+
 class Customer {
   String name;
   String code;
-  String avatar =
-      'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&s=200';
-  String registerAddress;
-  String tradePartner;
-  String phone;
-  String publicRelationsType;
-  String taxNo;
-  String legalUserName;
-  String businessContacts;
+  String address;
+  // String avatar =
+  //     'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&s=200';
+  // String registerAddress;
+  // String tradePartner;
+  // String phone;
+  // String publicRelationsType;
+  // String taxNo;
+  // String legalUserName;
+  // String businessContacts;
   List<Customer> customers;
 
-  Customer({this.name, this.code,this.avatar});
+  Customer({this.name, this.code,this.address});
 
   Customer.fromJSON(Map data) {
     
@@ -26,15 +29,48 @@ class Customer {
     print(customers.length);
   }
 
+  Customer.xml2List(outputxmlstr) {
+    List list = xml
+        .parse(outputxmlstr)
+        .findAllElements('response')
+        .map((node) => node.findElements('success').single.text)
+        .toList();
+        
+    if (list.length > 0) {
+      if (list[0] == "true") {
+        customers = xml
+            .parse(outputxmlstr)
+            .findAllElements('customer')
+            .map((node) => new Customer(name:
+                  node.findElements('title').single.text,code:
+                  node.findElements('code').single.text,address: 
+                  node.getAttribute("address")==null?node.findElements('code').single.text:''
+                ))
+            .toList();
+      } else {
+        customers = xml
+            .parse(outputxmlstr)
+            .findAllElements('response')
+            .map((node) => new Customer(name:
+                  'error',code:
+                  'error',address:
+                  node.findElements('error').single.text
+                ))
+            .toList();
+      }
+      // currentUser = loginUserList[0];
+    }
+  }
+
   Customer.get(Map data){
     this.code = data['code'];
     this.name = data['title'];
-    this.registerAddress = data['registerAddress'];
-    this.tradePartner= data['tradePartner'];
-    this.phone=data['phone'];
-    this.publicRelationsType=data['publicRelationsType'];
-    this.taxNo=data['tax_no'];
-    this.legalUserName=data['legalUserName'];
-    this.businessContacts=data['business_contacts'];
+    // this.registerAddress = data['registerAddress'];
+    // this.tradePartner= data['tradePartner'];
+    // this.phone=data['phone'];
+    // this.publicRelationsType=data['publicRelationsType'];
+    // this.taxNo=data['tax_no'];
+    // this.legalUserName=data['legalUserName'];
+    // this.businessContacts=data['business_contacts'];
   }
 }
--- lib/service/customer_api.dart
+++ lib/service/customer_api.dart
@@ -1,16 +1,63 @@
-import 'dart:io';
+import 'package:crypto/crypto.dart';
 import 'dart:convert';
 import '../models/customer.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
 
 class CustomerAPI {
-  Future<Customer> getCustomerList(int start) async {
-    var client = HttpClient();
-    var request = await client.getUrl(Uri.parse(
-        'http://10.0.65.171:8180/dataQuery/getCustomerListApi.do?title=%E5%86%80%E4%B8%9C&time=699&page=1&start=0&limit=20'));
-    var response = await request.close();
-    var responseBody = await response.transform(utf8.decoder).join();
-    Map<String,Object> data = json.decode(responseBody);
-    // print(data['rows']);
-    return Customer.fromJSON(data);
+
+  // Future<Customer> getCustomerList(int start) async {
+  //   var client = HttpClient();
+  //   var request = await client.getUrl(Uri.parse(
+  //       'http://10.0.65.171:8180/dataQuery/getCustomerListApi.do?title=%E5%86%80%E4%B8%9C&time=699&page=1&start=0&limit=20'));
+  //   var response = await request.close();
+  //   var responseBody = await response.transform(utf8.decoder).join();
+  //   Map<String,Object> data = json.decode(responseBody);
+  //   // print(data['rows']);
+  //   return Customer.fromJSON(data);
+  // }
+
+  // static Future<Customer> getCustomerList(String sales_code, String sales_office,String year) async {
+    Future<Customer> getCustomerList(int _page) async {
+    var date = new DateTime.now();
+    String timestamp =
+        "${date.year.toString()}${date.month.toString().padLeft(2, '0')}${date.day.toString().padLeft(2, '0')}${date.hour.toString().padLeft(2, '0')}${date.minute.toString().padLeft(2, '0')}${date.second.toString().padLeft(2, '0')}";
+    String md5Sign =
+        md5.convert(utf8.encode(timestamp + "WQWS")).toString().toUpperCase();
+    String inputxmlstr;
+    inputxmlstr = '''<![CDATA[<?xml version="1.0" encoding="UTF-8"?>
+            <data>
+              <sales_code>100</sales_code>
+              <sales_office>1011</sales_office>
+              <year>2019</year>
+            </data>]]>''';
+    String soap = '''
+        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sal="http://sales.open.ttx.io/">
+        <soap:Header/>
+        <soap:Body>
+            <sal:getCustomer>
+              <sal:inputTimestamp>$timestamp</sal:inputTimestamp>
+              <sal:inputSign>$md5Sign</sal:inputSign>
+              <sal:inputSecrect>249VOELF82CD</sal:inputSecrect>
+              <sal:inputXmlStr>$inputxmlstr</sal:inputXmlStr>
+            </sal:getCustomer>
+        </soap:Body>
+        </soap:Envelope>''';
+
+    var response = await http.post(
+        // Uri.parse("http://10.0.65.48:8287/services/customerApiServiceV1?wsdl"),http://192.168.0.106/
+        Uri.parse("http://192.168.0.106/sales/services/customerApiServiceV1?wsdl"),
+        // headers: getSAPHeader("Zif_WQ_IN_WS"),
+        body: utf8.encode(soap),
+        encoding: Encoding.getByName("UTF-8"));
+
+    if (response.statusCode != 200) {
+      print("Server Error !!!" + response.statusCode.toString());
+      return null;
+    }
+    var document = xml.parse(response.body);
+    var outputxmlstr = document.findAllElements('ns:return').single.text;
+    print(outputxmlstr);
+    return Customer.xml2List(outputxmlstr);
   }
 }
